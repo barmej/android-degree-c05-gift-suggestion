9bf98b67c5f2cea9869d612e47508d4ec162089c
oza <omar.albelbaisy@gmail.com>
2019-02-16 15:47:33 +0200

    Initial Commit

---
+++ app/src/main/java/com/barmej/giftsuggestion/MainActivity.java
@@ -0,0 +1,141 @@
+package com.barmej.giftsuggestion;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Constant for Log messages
+     */
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    /**
+     * Constant used as bundle key
+     */
+    private static final String BUNDLE_CURRENT_INDEX = "BUNDLE_CURRENT_INDEX";
+
+    /**
+     * Instance of Random class used to generate random index
+     */
+    private Random mRandom;
+
+    /**
+     * ImageView to display images
+     */
+    private ImageView mGiftImageView;
+
+    /**
+     * Array that hold drawable images ids
+     */
+    private int[] mGiftsPictures = {R.drawable.gift_1,
+            R.drawable.gift_2, R.drawable.gift_3, R.drawable.gift_4,
+            R.drawable.gift_5, R.drawable.gift_6, R.drawable.gift_7,
+            R.drawable.gift_8, R.drawable.gift_9, R.drawable.gift_10};
+
+    /**
+     * Variable used as index to move through images array
+     */
+    private int mCurrentIndex = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create new Random object
+        mRandom = new Random();
+        // Get imageView from view hierarchy
+        mGiftImageView = findViewById(R.id.image_gift);
+        Log.i(TAG, "Created");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Restarted");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Destroyed");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.containsKey(BUNDLE_CURRENT_INDEX)) {
+            mCurrentIndex = savedInstanceState.getInt(BUNDLE_CURRENT_INDEX);
+            if (mCurrentIndex != -1) {
+                showImage();
+            }
+        }
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(BUNDLE_CURRENT_INDEX, mCurrentIndex);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+
+    /**
+     * Called by android system when the button clicked
+     * It's specified as click handler using XML onClick attribute
+     *
+     * @param view the view that's being clicked
+     */
+    public void display(View view) {
+        // If counter does not exceed the last index in the array
+        if (mCurrentIndex < 9) {
+            // Generate random index
+            mCurrentIndex = mRandom.nextInt(10);
+            showImage();
+        } else {
+            // Reset the counter
+            mCurrentIndex = -1;
+        }
+    }
+
+    /**
+     * Show the image at the current index
+     */
+    private void showImage() {
+        Drawable giftDrawable = ContextCompat.getDrawable(this, mGiftsPictures[mCurrentIndex]);
+        mGiftImageView.setImageDrawable(giftDrawable);
+    }
+
+}
