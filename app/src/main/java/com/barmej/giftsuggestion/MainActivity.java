5a093ca2532e6bf0434a83c5899c1d381c836548
oza <omar.albelbaisy@gmail.com>
2019-02-16 19:41:56 +0200

    Use Gift object instead of two dimens array

--- app/src/main/java/com/barmej/giftsuggestion/MainActivity.java
+++ app/src/main/java/com/barmej/giftsuggestion/MainActivity.java
@@ -39,19 +39,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView mGiftNameTextView;
 
     /**
-     * Two dimens array that hold resources ids of gift pictures and names
+     * Array that hold gift objects
      */
-    private int[][] mGifts = {
-            {R.drawable.gift_1, R.string.damask_rose},
-            {R.drawable.gift_2, R.string.flower},
-            {R.drawable.gift_3, R.string.cake},
-            {R.drawable.gift_4, R.string.laptop},
-            {R.drawable.gift_5, R.string.mobile},
-            {R.drawable.gift_6, R.string.book},
-            {R.drawable.gift_7, R.string.piece_of_cake},
-            {R.drawable.gift_8, R.string.shirt},
-            {R.drawable.gift_9, R.string.shoe},
-            {R.drawable.gift_10, R.string.diamond}};
+    private Gift[] mGifts = new Gift[10];
 
     /**
      * Variable used as index to move through images array
@@ -68,6 +58,57 @@ public class MainActivity extends AppCompatActivity {
         mGiftImageView = findViewById(R.id.image_gift_picture);
         // Get TextView from view hierarchy
         mGiftNameTextView = findViewById(R.id.text_gift_name);
+
+        Gift gift1 = new Gift();
+        gift1.setName(R.string.damask_rose);
+        gift1.setPicture(R.drawable.gift_1);
+        mGifts[0] = gift1;
+
+        Gift gift2 = new Gift();
+        gift2.setName(R.string.flower);
+        gift2.setPicture(R.drawable.gift_2);
+        mGifts[1] = gift2;
+
+        Gift gift3 = new Gift();
+        gift3.setName(R.string.cake);
+        gift3.setPicture(R.drawable.gift_3);
+        mGifts[2] = gift3;
+
+        Gift gift4 = new Gift();
+        gift4.setName(R.string.laptop);
+        gift4.setPicture(R.drawable.gift_4);
+        mGifts[3] = gift4;
+
+        Gift gift5 = new Gift();
+        gift5.setName(R.string.mobile);
+        gift5.setPicture(R.drawable.gift_5);
+        mGifts[4] = gift5;
+
+        Gift gift6 = new Gift();
+        gift6.setName(R.string.book);
+        gift6.setPicture(R.drawable.gift_6);
+        mGifts[5] = gift6;
+
+        Gift gift7 = new Gift();
+        gift7.setName(R.string.piece_of_cake);
+        gift7.setPicture(R.drawable.gift_7);
+        mGifts[6] = gift7;
+
+        Gift gift8 = new Gift();
+        gift8.setName(R.string.shirt);
+        gift8.setPicture(R.drawable.gift_8);
+        mGifts[7] = gift8;
+
+        Gift gift9 = new Gift();
+        gift9.setName(R.string.shoe);
+        gift9.setPicture(R.drawable.gift_9);
+        mGifts[8] = gift9;
+
+        Gift gift10 = new Gift();
+        gift10.setName(R.string.diamond);
+        gift10.setPicture(R.drawable.gift_10);
+        mGifts[9] = gift10;
+
         Log.i(TAG, "Created");
     }
 
@@ -149,9 +190,10 @@ public class MainActivity extends AppCompatActivity {
      * Show the picture & name of the gift located at current index
      */
     private void showSuggestedGift() {
-        Drawable giftDrawable = ContextCompat.getDrawable(this, mGifts[mCurrentIndex][0]);
+        Gift suggestedGift = mGifts[mCurrentIndex];
+        Drawable giftDrawable = ContextCompat.getDrawable(this, suggestedGift.getPicture());
         mGiftImageView.setImageDrawable(giftDrawable);
-        mGiftNameTextView.setText(mGifts[mCurrentIndex][1]);
+        mGiftNameTextView.setText(suggestedGift.getName());
     }
 
 }
