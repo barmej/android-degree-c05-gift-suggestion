74f428a98e85f0225b8d3c43c2d922c042572601
oza <omar.albelbaisy@gmail.com>
2019-02-16 20:05:11 +0200

    Use Constructor instead of setters & getter to initialize Gift object attributes

--- app/src/main/java/com/barmej/giftsuggestion/Gift.java
+++ app/src/main/java/com/barmej/giftsuggestion/Gift.java
@@ -2,22 +2,54 @@ package com.barmej.giftsuggestion;
 
 public class Gift {
 
+    /**
+     * Integer variable that hold string resource id of gift name
+     */
     private int name;
+
+    /**
+     * Integer variable that hold drawable resource id of gift picture
+     */
     private int picture;
 
+    /**
+     * Constructor to initialize gift attributes
+     * @param name
+     * @param picture
+     */
+    public Gift(int name, int picture) {
+        this.name = name;
+        this.picture = picture;
+    }
 
+    /**
+     * Get string resource id of gift name
+     * @return string resource id
+     */
     public int getName() {
         return name;
     }
 
+    /**
+     * Set gift name string resource id
+     * @param name string resource id
+     */
     public void setName(int name) {
         this.name = name;
     }
 
+    /**
+     * Get drawable id of gift picture
+     * @return
+     */
     public int getPicture() {
         return picture;
     }
 
+    /**
+     * Set gift image drawable resource id
+     * @param picture
+     */
     public void setPicture(int picture) {
         this.picture = picture;
     }
