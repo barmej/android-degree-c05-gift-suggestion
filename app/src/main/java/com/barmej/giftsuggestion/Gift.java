e179f54a859c82ed06305209e922b86e9077eed3
oza <omar.albelbaisy@gmail.com>
2019-02-17 18:31:02 +0200

    Using Enum as gift type and change gift name color based on gift type

--- app/src/main/java/com/barmej/giftsuggestion/Gift.java
+++ app/src/main/java/com/barmej/giftsuggestion/Gift.java
@@ -2,6 +2,10 @@ package com.barmej.giftsuggestion;
 
 public class Gift {
 
+    public enum GiftType {
+        NATURAL, FOOD, CLOTHES, ELECTRONICS, JEWELRY, OTHER
+    }
+
     /**
      * Integer variable that hold string resource id of gift name
      */
@@ -13,13 +17,20 @@ public class Gift {
     private int picture;
 
     /**
+     * Enum variable that hold gift type
+     */
+    private GiftType giftType;
+
+    /**
      * Constructor to initialize gift attributes
      * @param name
      * @param picture
+     * @param giftType {@link com.barmej.giftsuggestion.Gift.GiftType}
      */
-    public Gift(int name, int picture) {
+    public Gift(int name, int picture, GiftType giftType) {
         this.name = name;
         this.picture = picture;
+        this.giftType = giftType;
     }
 
     /**
@@ -32,7 +43,7 @@ public class Gift {
 
     /**
      * Set gift name string resource id
-     * @param name string resource id
+     * @param name gift name string resource id
      */
     public void setName(int name) {
         this.name = name;
@@ -40,7 +51,7 @@ public class Gift {
 
     /**
      * Get drawable id of gift picture
-     * @return
+     * @return gift picture drawable id
      */
     public int getPicture() {
         return picture;
@@ -52,6 +63,22 @@ public class Gift {
      */
     public void setPicture(int picture) {
         this.picture = picture;
+    }
+
+    /**
+     * Get gift type {@link com.barmej.giftsuggestion.Gift.GiftType}
+     * @return gift type
+     */
+    public GiftType getGiftType() {
+        return giftType;
+    }
+
+    /**
+     * Set gift type {@link com.barmej.giftsuggestion.Gift.GiftType}
+     * @param giftType
+     */
+    public void setGiftType(GiftType giftType) {
+        this.giftType = giftType;
     }
 
 }
